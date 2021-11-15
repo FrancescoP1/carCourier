975be0e68e86ab5235b6c91eb45e9b99234d80df
unknown <francesco.petrovici@s.unibuc.ro>
2021-11-15 13:11:54 +0200

    prez

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
-<?php
-    //Get Heroku ClearDB connection information
-    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    $cleardb_server = $cleardb_url["host"];
-    $cleardb_username = $cleardb_url["user"];
-    $cleardb_password = $cleardb_url["pass"];
-    $cleardb_db = substr($cleardb_url["path"],1);
-    $active_group = 'default';
-    $query_builder = TRUE;
-    // Connect to DB
-    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-    echo "Hello World, Francesco here!";
-    echo "salkf, nabs";
-?>
+<html>
+    <?php
+        //Get Heroku ClearDB connection information
+        $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $cleardb_server = $cleardb_url["host"];
+        $cleardb_username = $cleardb_url["user"];
+        $cleardb_password = $cleardb_url["pass"];
+        $cleardb_db = substr($cleardb_url["path"],1);
+        $active_group = 'default';
+        $query_builder = TRUE;
+        // Connect to DB
+        $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+        
+    ?>
+    <h1>Car Courier</h1>
+    <h2>Prezentare aplicatie web</h2>
+
+</html>
+
+
